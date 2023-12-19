be6f73b15a366d54c15f2088e816911a84acb276
sainikhil1605 <sainikhilvatti1605@gmail.com>
2023-12-18 19:34:24 -0600

    Inital Setup

---
+++ src/test/java/com/nikhil/mockito/mockitopractice/MockitoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.mockito.mockitopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
