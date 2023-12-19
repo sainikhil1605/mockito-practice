0e2495ce2e8f12ec7e7403fade949c6fa82bc7fe
sainikhil1605 <sainikhilvatti1605@gmail.com>
2023-12-18 20:06:17 -0600

    Added Mock Annotation

--- src/test/java/com/nikhil/mockito/mockitopractice/business/SomeBusinessMockImplTest.java
+++ src/test/java/com/nikhil/mockito/mockitopractice/business/SomeBusinessMockImplTest.java
@@ -1,19 +1,29 @@
 package com.nikhil.mockito.mockitopractice.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockImplTest {
+
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
 
     @Test
     void test(){
-        DataService dataServiceMock=mock(DataService.class);
+//        DataService dataServiceMock=mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{1,2,3});
-        SomeBusinessImpl businessImpl=new SomeBusinessImpl(dataServiceMock);
+//        SomeBusinessImpl businessImpl=new SomeBusinessImpl(dataServiceMock);
         int result= businessImpl.findTheGreatestFromAllData();
         assertEquals(3,result);
     }
